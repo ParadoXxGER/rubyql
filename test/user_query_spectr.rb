1d61839ce6075319044169c565353b168466327c
Niklas Hanft <Niklas.Hanft@outlook.com>
2017-12-14 07:58:07 +0100

    Enhance testing

--- test/user_query_spectr.rb
+++ test/user_query_spectr.rb
@@ -1,18 +1,19 @@
 require 'spectr'
 require_relative '../lib/rubyql'
+require_relative '../lib/init_error'
 
-Spectr.new.test 'Test the queryable class' do |test|
-  class UserQuery < RubyQL
-    def query
-      { 'username' => 'paradoxxger',
-        'firstname' => 'Niklas',
-        'lastname' => 'Hanft',
-        'email' => 'niklas.hanft@outlook.com' }
-    end
+class UserQuery < RubyQL
+  def query
+    { 'username' => 'paradoxxger',
+      'firstname' => 'Niklas',
+      'lastname' => 'Hanft',
+      'email' => 'niklas.hanft@outlook.com' }
   end
+end
 
-  test.assume('The response is a hash', true) do
-    UserQuery.new('lastname' => '', 'email' => 'niklas.hanft@outlook.com').execute.is_a?(Hash)
+Spectr.new.test 'Test the UserQuery class' do |test|
+  test.assume('The response is a hash', Hash) do
+    UserQuery.new('lastname' => '', 'email' => 'niklas.hanft@outlook.com').execute.class
   end
 
   assumed_result = { 'firstname' => 'Niklas', 'lastname' => 'Hanft', 'email' => 'niklas.hanft@outlook.com' }
@@ -29,5 +30,23 @@ Spectr.new.test 'Test the queryable class' do |test|
 
   test.assume('The query_params are only the email', 'email' => 'niklas.hanft@outlook.com') do
     UserQuery.new('lastname' => '', 'email' => 'niklas.hanft@outlook.com').query_params
+  end
+end
+
+Spectr.new.test 'Test the wrong initialization of the UserQuery class' do |test|
+  test.assume('The initialization should fail with a String', InitError) do
+    begin
+      UserQuery.new('WrongAttribute')
+    rescue InitError => e
+      e.class
+    end
+  end
+
+  test.assume('The initialization should fail with an Integer', InitError) do
+    begin
+      UserQuery.new(1337)
+    rescue InitError => e
+      e.class
+    end
   end
 end
